d27b8b99e46c18f32d5d9ce6757c4a5613ccd6d6
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-05-08 18:15:43 -0300

    Snake body orbs follow head with physics

--- Assets/Code/SnakeFollow.cs
+++ Assets/Code/SnakeFollow.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class SnakeFollow : MonoBehaviour
 {
-
     public float MoveSpeed = 0.1f;
+    public GameObject SnakeHead;
 
-    private Vector2 mousePosition, selfSize;
+    private Vector2 mousePosition, snakeHeadSize;
     private Rect cameraRect;
 
     void Start()
     {
-        selfSize = GetComponent<Renderer>().bounds.size;
+        snakeHeadSize = SnakeHead.GetComponent<Renderer>().bounds.size;
         var bottomLeft = Camera.main.ScreenToWorldPoint(Vector3.zero);
         var topRight = Camera.main.ScreenToWorldPoint(new Vector3(
             Camera.main.pixelWidth, Camera.main.pixelHeight));
@@ -26,7 +26,7 @@ public class SnakeFollow : MonoBehaviour
 
     void Update()
     {
-        mousePosition = new Vector2(Mathf.Clamp(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, cameraRect.xMin + selfSize.x / 2, cameraRect.xMax - selfSize.y / 2), transform.position.y);
-        transform.position = Vector2.Lerp(transform.position, mousePosition, MoveSpeed);
+        mousePosition = new Vector2(Mathf.Clamp(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, cameraRect.xMin + snakeHeadSize.x / 2, cameraRect.xMax - snakeHeadSize.y / 2), SnakeHead.transform.position.y);
+        SnakeHead.transform.position = Vector2.Lerp(SnakeHead.transform.position, mousePosition, MoveSpeed);
     }
 }
