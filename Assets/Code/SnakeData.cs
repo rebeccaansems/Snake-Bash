d8997f674bf236cb3570d099e30129e2ac502dff
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-05-08 19:39:45 -0300

    Added text counter to orbs

---
+++ Assets/Code/SnakeData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SnakeData : MonoBehaviour
+{
+    public Text OrbCounter; 
+
+    private int currNumOrbs, maxNumOrbs;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (currNumOrbs != this.transform.childCount - 1)
+        {
+            currNumOrbs = this.transform.childCount - 1;
+            maxNumOrbs = Mathf.Max(currNumOrbs, maxNumOrbs);
+            OrbCounter.text = currNumOrbs.ToString();
+        }
+    }
+}
