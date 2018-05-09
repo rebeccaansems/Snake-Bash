ebf80ed10b4d58a42594a2ffa1111663e412d891
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-05-09 20:04:53 -0300

    Added box that costs orbs to break

---
+++ Assets/Code/BlockBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBox : MonoBehaviour
+{
+    public int OrbsRequired = 1;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            LoseOrb(collision.transform.parent.gameObject);
+        }
+    }
+
+    private void LoseOrb(GameObject parent)
+    {
+        for (int i = 0; i < OrbsRequired; i++)
+        {
+            parent.GetComponent<SnakeData>().RemoveOrb();
+        }
+        Destroy(this.gameObject);
+    }
+}
