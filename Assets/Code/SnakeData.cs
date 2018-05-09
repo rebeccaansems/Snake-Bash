f9897f84f1aa0103ff8cb3003676271a873b0f95
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-05-09 20:14:09 -0300

    Cleaned up code for updating text

--- Assets/Code/SnakeData.cs
+++ Assets/Code/SnakeData.cs
@@ -15,29 +15,30 @@ public class SnakeData : MonoBehaviour
     private void Start()
     {
         AllOrbs = this.transform.GetComponentsInChildren<HingeJoint2D>().Select(x => x.gameObject).ToList();
+        UpdateText();
     }
 
-    private void Update()
+    private void UpdateText()
     {
-        if (currNumOrbs != this.transform.childCount - 1)
-        {
-            currNumOrbs = this.transform.childCount - 1;
-            maxNumOrbs = Mathf.Max(currNumOrbs, maxNumOrbs);
-            OrbCounter.text = currNumOrbs.ToString();
-        }
+        maxNumOrbs = Mathf.Max(AllOrbs.Count, maxNumOrbs);
+        OrbCounter.text = AllOrbs.Count.ToString();
     }
 
     public void AddOrb()
     {
         var newOrb = Instantiate(OrbPrefab, this.transform);
-        newOrb.transform.position = new Vector2(AllOrbs[currNumOrbs - 1].transform.position.x, AllOrbs[currNumOrbs - 1].transform.position.y);
-        newOrb.GetComponent<HingeJoint2D>().connectedBody = AllOrbs[currNumOrbs - 1].GetComponent<Rigidbody2D>();
+        newOrb.transform.position = new Vector2(AllOrbs[AllOrbs.Count - 1].transform.position.x, AllOrbs[AllOrbs.Count - 1].transform.position.y);
+        newOrb.GetComponent<HingeJoint2D>().connectedBody = AllOrbs[AllOrbs.Count - 1].GetComponent<Rigidbody2D>();
         AllOrbs.Add(newOrb);
+
+        UpdateText();
     }
 
     public void RemoveOrb()
     {
-        Destroy(AllOrbs[currNumOrbs - 1].gameObject);
-        AllOrbs.RemoveAt(currNumOrbs - 1);
+        Destroy(AllOrbs[AllOrbs.Count - 1].gameObject);
+        AllOrbs.RemoveAt(AllOrbs.Count - 1);
+
+        UpdateText();
     }
 }
